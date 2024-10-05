4fc7df8f5a8a544bb9114b5ad80c975c225d046a
mithat <mithatutr473@gmail.com>
2024-10-05 21:47:52 +0300

    a

--- project.js
+++ project.js
@@ -1,25 +1,21 @@
 const prompt = require("prompt-sync")();
 
-const ROWS  = 3;
+const ROWS = 3;
 const COLS = 3;
 
 const SYMBOLS_COUNT = {
-  "A": 2,
-  "B": 4,
-  "C": 6,
-  "D": 8
-}
+  A: 2,
+  B: 4,
+  C: 6,
+  D: 8,
+};
 
 const SYMBOL_VALUES = {
-  "A": 5,
-  "B": 4,
-  "C": 3,
-  "D": 2
-}
-
-
-
-
+  A: 5,
+  B: 4,
+  C: 3,
+  D: 2,
+};
 
 const deposit = () => {
   while (true) {
@@ -47,12 +43,12 @@ const getNumberOfLines = () => {
   }
 };
 
-const getBet = (balance,lines) => {
+const getBet = (balance, lines) => {
   while (true) {
     const bet = prompt("Enter the bet per line: ");
     const numberBet = parseFloat(bet);
 
-    if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance/lines) {
+    if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
       console.log("Invalid bet, try again");
     } else {
       return numberBet;
@@ -60,14 +56,25 @@ const getBet = (balance,lines) => {
   }
 };
 
-
 const spin = () => {
   const symbols = [];
-  for (const [symbol,count] of Object.entries(SYMBOLS_COUNT)){
-    
+  for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+    for (let i = 0; i < count; i++) {
+      symbols.push(symbol);
+    }
+  }
+};
+
+const reels = [[], [], []];
+for (let i = 0; i<COLS; i++){
+    const reelSymbols = [...symbols];
+  for (let j = 0; j<ROWS; j++){
+        const randomIndex = (Math.random() * reelSymbols.lenght)   
+      const selectedSymbol = reelSymbols[randomIndex];
   }
 }
 
+spin();
 let balance = deposit();
 const numberOfLines = getNumberOfLines;
-const bet = getBet(balance,numberOfLines);
+const bet = getBet(balance, numberOfLines);
