5866df9fe2ae4854e028fe16d9332d1ea795df14
mithat <mithatutr473@gmail.com>
2024-10-05 21:30:45 +0300

    aa

--- project.js
+++ project.js
@@ -1,5 +1,26 @@
 const prompt = require("prompt-sync")();
 
+const ROWS  = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+  "A": 2,
+  "B": 4,
+  "C": 6,
+  "D": 8
+}
+
+const SYMBOL_VALUES = {
+  "A": 5,
+  "B": 4,
+  "C": 3,
+  "D": 2
+}
+
+
+
+
+
 const deposit = () => {
   while (true) {
     const depositAmount = prompt("Enter a deposit amount: ");
@@ -38,6 +59,14 @@ const getBet = (balance,lines) => {
     }
   }
 };
+
+
+const spin = () => {
+  const symbols = [];
+  for (const [symbol,count] of Object.entries(SYMBOLS_COUNT)){
+    
+  }
+}
 
 let balance = deposit();
 const numberOfLines = getNumberOfLines;
